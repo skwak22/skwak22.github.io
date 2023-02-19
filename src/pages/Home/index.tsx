8d4ee4a4e5bf3831a80ca53f3ccdc2c8fca3d38c
skwak22 <skwak22@amherst.edu>
2023-02-19 18:57:58 -0500

    new react app

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,28 @@
+
+export const Home = () => {
+  return (
+    <body>
+      <div id="me" className="center">
+        <a href="./about.html">
+          {" "}
+          <img
+            className="circle center"
+            src={require("../../assets/IMG_2915.png")}
+            alt="Sunghoon Kwak"
+          />
+        </a>
+      </div>
+      <div id="description">
+        Hello, and welcome to my website. My name is Sunghoon Kwak, and I am a
+        recent graduate at Amherst College. At Amherst, I studied Computer Science and Philosophy. 
+        <br />
+        <br />
+        I'm currently working as a Software Engineer at{" "}
+        <a href="https://www.angellist.com/"> AngelList </a> in NYC.
+        ✌🏼
+      </div>
+    </body>
+  );
+};
+
+export default Home;
